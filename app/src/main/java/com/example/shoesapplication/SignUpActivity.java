6aff5502a8d24334e24698a712b8997d30c59fad
IloveEdelgard <128874191+IloveEdelgard@users.noreply.github.com>
2023-12-11 10:30:20 +0700

    Add SignIn

--- app/src/main/java/com/example/shoesapplication/SignUpActivity.java
+++ app/src/main/java/com/example/shoesapplication/SignUpActivity.java
@@ -1,19 +1,36 @@
 package com.example.shoesapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import javax.inject.Singleton;
 
 public class SignUpActivity extends AppCompatActivity {
     TextView txtlogin;
+    private EditText editUserName, editPassWord, editConfirmPassword;
+    private AppCompatButton signupBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        editUserName = findViewById(R.id.usernameEdit);
+        editPassWord = findViewById(R.id.passwordEdit);
+        editConfirmPassword = findViewById(R.id.rePasswordEdit);
+        signupBtn = findViewById(R.id.signInBtn);
+
 
         txtlogin = findViewById(R.id.signupTextsuggest);
         txtlogin.setOnClickListener(new View.OnClickListener() {
@@ -23,5 +40,21 @@ public class SignUpActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        signupBtn.setOnClickListener(v-> {
+            String userName = getValue(editUserName);
+            String password = getValue(editPassWord);
+            String confirmPassword = getValue(editConfirmPassword);
+            if(password.equals(confirmPassword)) {
+                User  user =  User.signUp(SignUpActivity.this,new User(userName, password));
+                if(user != null) {
+                    Intent intent = new Intent(SignUpActivity.this, account.class );
+                    startActivity(intent);
+                }
+            }
+        });
+    }
+
+    private String getValue(EditText editText) {
+        return String.valueOf(editText.getText()).trim();
     }
 }
---
+++ app/src/main/java/com/example/shoesapplication/User.java
@@ -0,0 +1,63 @@
+package com.example.shoesapplication;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.gson.Gson;
+
+public class User {
+    private String userName;
+    private String password;
+
+    public User() {
+    }
+
+    public User(String userName, String password) {
+        this.userName = userName;
+        this.password = password;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public static User signUp(Context context, User user) {
+        if(user == null) return null;
+        FirebaseDatabase db = FirebaseDatabase.getInstance();
+        db.getReference("users").push().setValue(user);
+
+        SharedPreferences sharedPreferences = context.getSharedPreferences("auth_info",Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("user", new Gson().toJson(user));
+        editor.apply();
+        return user;
+    }
+
+    public static User getUser(Context context) {
+        SharedPreferences sharedPreferences = context.getSharedPreferences("auth_info",Context.MODE_PRIVATE);
+        String userJson = sharedPreferences.getString("user", null);
+        if(userJson == null) return null;
+        return new Gson().fromJson(userJson, User.class);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userName='" + userName + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
